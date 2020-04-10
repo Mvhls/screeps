2740d2b8b63c02f048f4e7ec213a4f80732cdb55
Vern <mvernhein@gmail.com>
2020-04-09 23:10:41 -0500

    added comments around what I think a site would be

--- src/sites.js
+++ src/sites.js
@@ -1,5 +1,8 @@
+// sites are the declarative data for the game. It describes your room, or a part of
+// a room called a 'site'
 var site = {
     type: "room",
+    name: "first room site",
     spawnId: "e644dae84a8f462106ea4e71", // id of creep spawner for site
     controllerId: "2a21e1b53d802c9bcb5ee3b1", // id of closest controller, if any
     
@@ -8,7 +11,8 @@ var site = {
     // doesn't need to be every structure? possibly just important ones used in jobs
     structures: [
         {
-            name: "container",
+            name: "container-1",
+            type: "container",
             cords: {
                 x: 33,
                 y: 22
@@ -19,7 +23,7 @@ var site = {
     // jobs should list required fields or options. like a 'harvest, then deliver to' job would require a delivery spot
     jobs: [
         {
-            name: "basicBitchHarvester", // name of function
+            name: "basicHarvester", // name of function
             screepCount: 2, // max screeps
             default: "upgrade" // possibly include a default job for the screep, if there's nothing to do
         }
